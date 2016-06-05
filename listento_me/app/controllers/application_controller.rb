9f5a33332e9d1a3de7762a1f679f63342a1998f0
risurisu <lizdorval@gmail.com>
2016-06-05 14:50:02 -0400

    adds episode model and associations

--- listento_me/app/controllers/application_controller.rb
+++ listento_me/app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-end
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+
+# this mumbo jumbo lets you save edited stuff with devise
+
+
+   def configure_permitted_parameters
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :title])
+     devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password,
+     :password_confirmation, :current_password, :title, :description, :itunes, :stitcher, :podbay])
+   end
+
+   end
--- listento_me/db/schema.rb
+++ listento_me/db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160604191311) do
+ActiveRecord::Schema.define(version: 20160605184510) do
+
+  create_table "episodes", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "podcast_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "episodes", ["podcast_id"], name: "index_episodes_on_podcast_id"
 
   create_table "podcasts", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -26,6 +36,11 @@ ActiveRecord::Schema.define(version: 20160604191311) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "title"
+    t.text     "description"
+    t.string   "itunes"
+    t.string   "stitcher"
+    t.string   "podbay"
   end
 
   add_index "podcasts", ["email"], name: "index_podcasts_on_email", unique: true
