2f520af595279413996d15d59a24f3eec5a92238
risurisu <lizdorval@gmail.com>
2016-06-05 21:18:52 -0400

    set up pagination and fixed stuff i broke

--- listento_me/app/controllers/episodes_controller.rb
+++ listento_me/app/controllers/episodes_controller.rb
@@ -19,7 +19,7 @@ before_action :find_episode, only: [:show, :edit, :update, :destroy]
   end
 
   def show
-    @episodes = Episode.where(podcast_id: @podcast).order("created_at DESC").reject { |e| e.id = @episode.id }
+    @episodes = Episode.where(podcast_id: @podcast).order("created_at DESC").limit(3).reject { |e| e.id = @episode.id }
   end
 
   def edit
